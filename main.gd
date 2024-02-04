a50251d70996dd3303429308f3379c6e540abc30
ddmitriev <ddmitriev@intermedia.net>
2024-02-04 15:54:29 +0300

    test planet 2

--- main.gd
+++ main.gd
@@ -1,13 +1,16 @@
 extends Node2D
 var asteroid
+var asteroid2
 var player
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	asteroid = $asteroid
+	asteroid2 = $asteroid2
 	player = $player
 	player.gravityObjects.append(asteroid)
+	player.gravityObjects.append(asteroid2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
